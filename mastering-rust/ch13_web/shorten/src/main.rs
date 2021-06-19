c0bfeffd26c9c83a0b33f377f901434b5954f17b
ShuLiang <shuliang@live.cn>
2021-06-19 16:11:07 +0800

    Add 《精通rust》shorten

---
+++ mastering-rust/ch13_web/shorten/src/main.rs
@@ -0,0 +1,27 @@
+use quicli::prelude::*;
+use reqwest::Client;
+use structopt::StructOpt;
+
+const HYPERURL_ADDR: &str = "127.0.0.1:3002";
+const HTTP_PREFIX: &str = "http://";
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    #[structopt(long = "url", short = "u")]
+    url: String,
+    #[structopt(flatten)]
+    verbosity: Verbosity,
+}
+
+fn main() -> CliResult {
+    let args = Cli::from_args();
+    println!("Shortening: {}", args.url);
+    let client = Client::new();
+    let mut res = client
+        .post(&format!("{}{}/shorten", HTTP_PREFIX, HYPERURL_ADDR))
+        .body(args.url)
+        .send()?;
+    let a: String = res.text().unwrap();
+    println!("{}{}", HTTP_PREFIX, a);
+    Ok(())
+}
